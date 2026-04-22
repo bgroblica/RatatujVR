a18bf3d3c9a8f106edf3259c6f995ca0baad3cd0
Bartosz Groblica <bgroblica@edu.cdv.pl>
2026-04-22 19:57:13 +0200

    Egg Crack

--- Assets/Scripts/Egg.cs
+++ Assets/Scripts/Egg.cs
@@ -2,26 +2,79 @@ using UnityEngine;
 
 public class Egg : Solids
 {
+    public enum EggState
+    {
+        Whole,
+        Cracked
+    }
+
+    [Header("Mesh")]
+    public MeshFilter meshFilter;
+    public Mesh wholeEgg;
+    public Mesh crackedEgg;
+
+    public GameObject eggShellPrefab;
+
+    public float crackForce = 2f;
+
     public float crackVelocity = 1.5f;
-    public float eggAmount = 1f;
 
     private Rigidbody rb;
+    private EggState eggState = EggState.Whole;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        SetState(EggState.Whole);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (rb.linearVelocity.magnitude < crackVelocity) return;
+        if (eggState != EggState.Whole) return;
 
-        Bowl bowl = collision.collider.GetComponentInParent<Bowl>();
+        if (rb.linearVelocity.magnitude < crackVelocity)
+            return;
 
-        if (bowl != null)
+        SetState(EggState.Cracked);
+        Debug.Log("Egg Cracked");
+    }
+
+    private void SetState(EggState newState)
+    {
+        eggState = newState;
+        UpdateVisuals();
+    }
+
+    private void SpawnShells()
+    {
+        if (eggShellPrefab == null) return;
+
+        GameObject shell = Instantiate(
+            eggShellPrefab,
+            transform.position,
+            transform.rotation
+        );
+
+        Rigidbody[] bodies = shell.GetComponentsInChildren<Rigidbody>();
+
+        foreach (var rb in bodies)
         {
-            bowl.AddEgg(eggAmount);
-            Destroy(gameObject);
+            rb.AddForce(transform.forward * crackForce, ForceMode.Impulse);
+        }
+    }
+
+    private void UpdateVisuals()
+    {
+        switch (eggState)
+        {
+            case EggState.Whole:
+                meshFilter.mesh = wholeEgg;
+                break;
+
+            case EggState.Cracked:
+                meshFilter.mesh = crackedEgg;
+                SpawnShells();
+                break;
         }
     }
 }
